30288e47860b2919e3f13b0c3833baa639b9b00d
Sean Chavez <realseanchavez@gmail.com>
2019-01-01 21:06:01 -0800

    Implement Blockchain class

--- simpleChain.js
+++ simpleChain.js
@@ -1,3 +1,5 @@
+SHA256 = require('crypto-js/sha256');
+
 class Block {
   constructor(data) {
     this.height = '';
@@ -8,5 +10,25 @@ class Block {
   }
 }
 
+class Blockchain {
+  constructor() {
+    this.chain = [];
+    this.addBlock(new Block('Genesis Block'));
+  }
+
+  addBlock(newBlock) {
+    if (this.chain.length > 0) {
+      newBlock.previousHash = this.chain[this.chain.length - 1].hash;
+    }
+    newBlock.previousHash = SHA256(JSON.stringify(newBlock)).toString();
+    this.chain.push(newBlock);
+  }
+}
+
 const wow = new Block('Seriously Fucking Stupid');
-console.log(wow);
+//console.log(wow);
+const bc = new Blockchain();
+bc.addBlock(wow);
+bc.addBlock(new Block('OMG'));
+bc.addBlock(new Block('So Bad'));
+console.log(bc.chain);
